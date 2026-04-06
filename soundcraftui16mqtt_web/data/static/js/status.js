cbfa6f2094ef3728bcd950aa39e6a2733349c25c
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-06 11:51:38 +0200

    WIP: web

--- soundcraftui16mqtt_web/data/static/js/status.js
+++ soundcraftui16mqtt_web/data/static/js/status.js
@@ -2,26 +2,28 @@ window.addEventListener("load", function(){
   broker_host = $("#mqtt_data").attr("host");
   broker_port = $("#mqtt_data").attr("port");
   var client = new Paho.Client(broker_host, Number(broker_port), uuid4());
+  window.mqtt_client = client;
 
   // set callback handlers
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
+  window.client.onConnectionLost = onConnectionLost;
+  window.client.onMessageArrived = onMessageArrived;
 
   // connect the client
-  client.connect({onSuccess:onConnect});
+  window.client.connect({onSuccess:onConnect});
+
 });
 
 function send_message(msg, topic){
   var message = new Paho.Message(msg);
-  message.destionationName = "database_request/" + client.clientId + "/" + topic;
-  client.send(message);
+  message.destionationName = "database_request/" + window.client.clientId + "/" + topic;
+  window.client.send(message);
 }
 
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
-  client.subscribe("database_update/all/#");
-  client.subscribe("database_update/" + client.clientId + "/#");
-  client.subscribe("webpage/#");
+  window.client.subscribe("database_update/all/#");
+  window.client.subscribe("database_update/" + window.client.clientId + "/#");
+  window.client.subscribe("webpage/#");
   console.log("Mqtt Client connected");
   $("#btn-mqtt").removeClass("btn-danger").addClass("btn-success");
 }
