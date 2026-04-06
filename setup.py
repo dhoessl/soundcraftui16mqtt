bcbae4d5c688d4dace933cfd17d836099504a954
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-06 12:05:23 +0200

    WIP: web

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="soundcraftui16mqtt",
-    version="0.0.1-rc22",
+    version="0.0.1-rc23",
     description=(
         "library for soundcraft ui16 project including all modules for mqtt "
         "connection"
