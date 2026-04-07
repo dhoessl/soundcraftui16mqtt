1b5ca99323305931a14c88d10d63a2b4424fa48d
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-07 16:44:14 +0200

    add endpoint and status update/sending/requesting to generic database listener

--- soundcraftui16mqtt_listeners/database.py
+++ soundcraftui16mqtt_listeners/database.py
@@ -7,11 +7,20 @@ from soundcraftui16mqtt_mqtt import MqttClient
 class DatabaseMqttListener(MqttClient):
     def __init__(
             self, run_forever: bool = False, host: str = "localhost",
-            port: int = 1883
+            port: int = 1883, enable_status: bool = False,
+            enable_endpoint: bool = False
     ) -> None:
         super().__init__(run_forever, host, port)
         self.request_topic = "database_request"
         self.update_topic = "database_update"
+        self.request_status_topic = "status_request"
+        self.send_status_topic = "status_report"
+        self.update_status_topic = "status_update"
+        self.request_endpoint_topic = "endpoint_request"
+        self.send_endpoint_topic = "endpoint_report"
+        self.update_endpoint_topic = "endpoint_update"
+        self.enable_status_updates = enable_status
+        self.enable_endpoint_updates = enable_endpoint
 
     def _on_connect(self, client, userdata, flags, reason, prop) -> None:
         self.client.subscribe(f"{self.update_topic}/all/#")
@@ -20,6 +29,22 @@ class DatabaseMqttListener(MqttClient):
         logger.debug(
             f"Listener Client connected to {self.update_topic}/{self.id}/#"
         )
+        if self.enable_status_updates:
+            self.client.subscribe(f"{self.update_status_topic}/all")
+            self.client.subscribe(f"{self.update_status_topic}/{self.id}")
+            logger.debug(
+                "Listener Client connected to "
+                f"{self.update_status_topic}/{self.id} and "
+                f"{self.update_status_topic}/all"
+            )
+        if self.enable_endpoint_updates:
+            self.client.subscribe(f"{self.update_endpoint_topic}/all")
+            self.client.subscribe(f"{self.update_endpoint_topic}/{self.id}")
+            logger.debug(
+                "Listener Client connected to "
+                f"{self.update_endpoint_topic}/{self.id} and "
+                f"{self.update_endpoint_topic}/all"
+            )
 
     def _on_message(self, client, userdata, msg) -> None:
         topic = msg.topic
@@ -39,8 +64,12 @@ class DatabaseMqttListener(MqttClient):
                 logger.debug(
                     f"Unsolved: {topic} => {decoded_msg}"
                 )
-            return None
-        logger.debug(f"Unsolved: {topic} => {decoded_msg}")
+        elif topic.startswith(self.update_status_topic):
+            self.status_update(decoded_msg)
+        elif topic.startswith(self.update_endpoint_topic):
+            self.endpoint_update(decoded_msg)
+        else:
+            logger.debug(f"Unsolved: {topic} => {decoded_msg}")
 
     def channel_update(self, msg: dict) -> None:
         logger.warning("Please set a channel_update(msg) function")
@@ -60,6 +89,14 @@ class DatabaseMqttListener(MqttClient):
 
     def bpm_update(self, msg: str) -> None:
         logger.warning("Please set a bpm_update(msg) function")
+        return None
+
+    def status_update(self, msg: dict) -> None:
+        logger.warning("Please add a status_update(msg) function")
+        return None
+
+    def endpoint_update(self, msg: dict) -> None:
+        logger.warning("Please add a endpoint_update(msg) function")
         return None
 
     def req_channel_update(self, param: str, channel: int | str) -> None:
@@ -108,4 +145,35 @@ class DatabaseMqttListener(MqttClient):
         self.client.publish(
             path.join(self.request_topic, self.id, "bpm"),
             ""
+        )
+
+    def req_status(self) -> None:
+        self.client.publish(
+            path.join(self.request_status_topic, self.id),
+            ""
+        )
+
+    def req_endpoints(self) -> None:
+        self.client.publish(
+            path.join(self.request_endpoint_topic, self.id),
+            ""
+        )
+
+    def send_status(self, entity: str, state: bool) -> None:
+        self.client.publish(
+            path.join(self.send_status_topic, self.id),
+            self._encode_message({
+                "state": state,
+                "name": entity
+            })
+        )
+
+    def send_endpoint(self, entity: str, address: str, port: int) -> None:
+        self.client.publish(
+            path.join(self.send_endpoint_topic, self.id),
+            self._encode_message({
+                "name": entity,
+                "address": address,
+                "port": port
+            })
         )
