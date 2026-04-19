50d103dcb6010e1a41c3cfe27fbee11758584973
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-19 22:02:46 +0200

    v0.0.2 first working state between database, mixer and mqtt
    * Database with config values is setup
    * MixerListener is able to connect to mixer and send messages to
      database
    * MixerSender is able to connect
    * mqtt is working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="soundcraftui16mqtt",
-    version="0.0.1-rc35",
+    version="0.0.2",
     description=(
         "library for soundcraft ui16 project including all modules for mqtt "
         "connection"
