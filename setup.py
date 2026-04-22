da596c64ced5c63da84d4a7800f472da7770e75b
Dominic Hößl <dominic.hoessl@noris.de>
2026-04-23 01:15:48 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="soundcraftui16mqtt",
-    version="0.0.4",
+    version="0.0.4-rc1",
     description=(
         "library for soundcraft ui16 project including all modules for mqtt "
         "connection"
